c1d4f3e848d3f7fa2a9abab8bd5868ede6b0f90c
yorammi <yorammi@tikalk.com>
2019-01-04 01:04:27 +0200

    add build of tci-master [Branch] master

---
+++ src/scripts/tci-master-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p temp
+cd temp
+git clone git@github.com:TikalCI/tci-bloody-jenkins.git
+cd tci-bloody-jenkins
+git checkout master
+
+cp plugins.txt ..
+cat ../../src/resources/tci/tci-extra-plugins.txt >> ../plugins.txt
+cat ../plugins.txt | sort > plugins.txt
+docker build tci-master .
+
+cd ../..
+rm -rf temp/tci-bloody-jenkins
