f912190ab9e4bf7d5dd49f2e01cd2f3eceb8da28
yorammi <yorammi@tikalk.com>
2019-01-04 00:37:39 +0200

    Add sync script [Branch] master

--- src/scripts/syncUpstream.sh
+++ src/scripts/syncUpstream.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+mkdir -p temp
+cd temp
+git clone git@github.com:TikalCI/tci-bloody-jenkins.git
+cd tci-bloody-jenkins
+git checkout master
 git remote add upstream git@github.com:odavid/my-bloody-jenkins.git
 git fetch upstream
-git checkout master
 git merge upstream/master
 git push origin HEAD
 git remote remove upstream
+cd ../..
+rm -rf temp/tci-bloody-jenkins
