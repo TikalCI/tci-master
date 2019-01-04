f565faf05068320f7a680555cdb0b18871c1ce6c
yorammi <yorammi@tikalk.com>
2019-01-04 03:32:35 +0200

    Support a configurable tci-master image [Branch] master

--- src/scripts/tci-master-build.sh
+++ src/scripts/tci-master-build.sh
@@ -2,16 +2,34 @@
 
 set -e
 
+if [[ $# > 0 ]]; then
+    version=$1
+else
+    echo "you must provide a version as parameter"
+    exit 1
+fi
 mkdir -p temp
 cd temp
 git clone git@github.com:TikalCI/tci-bloody-jenkins.git
 cd tci-bloody-jenkins
 git checkout master
+cp plugins.txt ../origin-plguins.txt
 
-cp plugins.txt ..
+cp ../origin-plguins.txt ../plugins.txt
 cat ../../src/resources/tci/tci-extra-plugins.txt >> ../plugins.txt
 cat ../plugins.txt | sort > plugins.txt
-docker build -t tci-master .
+docker build -t tikalci/tci-master-minimal:latest .
+docker tag tikalci/tci-master-minimal:latest tikalci/tci-master-minimal:$version
+docker push tikalci/tci-master-minimal:latest
+docker push tikalci/tci-master-minimal:$version
+
+cp ../origin-plguins.txt ../plugins.txt
+cat ../../src/resources/tci/tci-full-extra-plugins.txt >> ../plugins.txt
+cat ../plugins.txt | sort > plugins.txt
+docker build -t tikalci/tci-master-full:latest .
+docker tag tikalci/tci-master-full:latest tikalci/tci-master-full:$version
+docker push tikalci/tci-master-full:latest
+docker push tikalci/tci-master-full:$version
 
 cd ../..
 rm -rf temp/tci-bloody-jenkins
