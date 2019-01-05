fc6539cd10a15de0fb05c20d9fa782c2a2edf7c9
yorammi <yorammi@tikalk.com>
2019-01-05 07:49:43 +0200

    add set-lts script [Branch] master

--- src/scripts/set-lts.sh
+++ src/scripts/set-lts.sh
@@ -4,4 +4,7 @@ set -e
 
 docker tag tikalci/tci-master-minimal:latest tikalci/tci-master-minimal:lts
 docker tag tikalci/tci-master-full:latest tikalci/tci-master-full:lts
+docker push tikalci/tci-master-minimal:lts
+docker push tikalci/tci-master-full:lts
+
 
