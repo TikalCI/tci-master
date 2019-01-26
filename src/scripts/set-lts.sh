317ad4fa126f97087ffc7003007bf926563b3658
yorammi <yorammi@tikalk.com>
2019-01-26 21:58:38 +0200

    ajdust scripts to tci-bloody-master changes

--- src/scripts/set-lts.sh
+++ src/scripts/set-lts.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-docker tag tikalci/tci-master-minimal:latest tikalci/tci-master-minimal:lts
-docker tag tikalci/tci-master-full:latest tikalci/tci-master-full:lts
-docker push tikalci/tci-master-minimal:lts
-docker push tikalci/tci-master-full:lts
+TYPE=$1
+
+docker tag tikalci/tci-master-${TYPE}:latest tikalci/tci-master-${TYPE}:lts
+docker push tikalci/tci-master-${TYPE}:lts
 
 
